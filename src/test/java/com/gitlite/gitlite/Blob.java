fde9b7e75506ca91617556598b34cea4ef59e627
shreenidhi2205 <shreenidhirithe2210@gmail.com>
2026-03-30 18:16:31 +0530

    Initial commit : SpringBoot and Dependencies setup

---
+++ src/test/java/com/gitlite/gitlite/Blob.java
@@ -0,0 +1,4 @@
+package com.gitlite.gitlite;
+
+public class Blob {
+}
---
+++ src/test/java/com/gitlite/gitlite/GitliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlite.gitlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
